1e9f1602ed792ce985cd2e0c9c3ab81eddd38861
yuriitish@mail.ru <yuriitish@mail.ru>
2023-04-13 18:54:06 +0400

    13.04 commit

--- components/videoplace.js
+++ components/videoplace.js
@@ -7,60 +7,166 @@ export default function Videoplace() {
       <section className={styles.section}>
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
         </div>
         {/*  */}
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
         </div>
         {/*  */}
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
         </div>
         {/*  */}
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
         </div>
         {/*  */}
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
         </div>{" "}
         {/*  */}
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
         </div>
         {/*  */}
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
-        </div>{" "}
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
+        </div>
+        {" "}
         {/*  */}
         <div className={styles.video__div}>
           <Image src="/videoImage.png" width={366} height={214} />
-          <h1>Название видеоролика</h1>
-          <h3>Канал</h3>
-          <p>1 день назад</p>
-        </div>{" "}
-        
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
+        </div>
+        {" "}
+        <div className={styles.video__div}>
+          <Image src="/videoImage.png" width={366} height={214} />
+          <div className={styles.videoplaceDescription}>
+            <div className={styles.accountImage}>
+              <Image
+                src="/accountImage.png"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div>
+              <h1 className={styles.videoplaceH1}>Название видеоролика</h1>
+              <h3 className={styles.videoplaceH3}>Канал</h3>
+              <p className={styles.videoplaceP}>1 день назад</p>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
